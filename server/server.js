d010c4092e409cef28f362a8769580f947243b84
Sam Corcos <samcorcos@gmail.com>
2015-01-21 18:42:52 -0500

    cheerio might be working

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 Meteor.methods({
 	getMenus: function(){
-    var cheerio = Meteor.npmRequire("cheerio")
-    var $ = cheerio.load(results);
+    var cheerio = Meteor.npmRequire("cheerio");
+    console.log(cheerio);
+
 		var results = Meteor.http.call("GET","https://aspc.pomona.edu/menu/");
+
+    var $ = cheerio.html(results);
+
     return results;
 	}
 });
