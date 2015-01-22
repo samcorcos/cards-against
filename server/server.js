572719378b96bd585fc9284881698fa448063aee
Sam Corcos <samcorcos@gmail.com>
2015-01-21 20:25:33 -0500

    adding menus collection

--- server/server.js
+++ server/server.js
@@ -2,11 +2,24 @@ Meteor.methods({
 	getMenus: function(){
 		var results = Meteor.http.get("https://aspc.pomona.edu/menu/", {timeout: 30000});
     var html = results.content;
-
     $ = cheerio.load(html)
 
-    var table = $("#meal_header");
+		// These values are hard-coded
+		var meals = ["Breakfast", "Lunch", "Dinner"];
+
+		// These will be the IDs we will iterate over to get the menu items.
+		var diningHalls = ["frank_menu", "frary_menu", "oldenborg_menu", "cmc_menu", "scripps_menu", "pitzer_menu", "mudd_menu"];
+
+    var table = $("#meal_header>th").text();
     console.log(table);
+
+
+		var parseMenu = function(diningHalls) {
+			
+		}
+
+
+
 
     return html;
 	}
